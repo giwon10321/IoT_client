38c13e7d69a311101a934b56f465bd0545cba4d5
kwongiwon <kwongiwon@kwongiwonui-iMac.local>
2015-03-10 21:13:29 +0900

    150310 commit

--- temperature_sensor.js
+++ temperature_sensor.js
@@ -1,10 +1,16 @@
 var util = require('util'),
     events = require('events').EventEmitter;
 
-function TemperatureSensor(){
+var properTemp = 22;
+var range = 5;
+
+function TemperatureSensor(temp, type){
     if( false == (this instanceof TemperatureSensor)){
         new TemperatureSensor();
     }
+    this.temp = temp;
+    this.type = type;
+    this.state = 'normal';
     events.call(this);
 }
 
@@ -12,14 +18,31 @@ util.inherits(TemperatureSensor, events);
 
 TemperatureSensor.prototype.start = function(){
     var self = this;
-    var currentTemp = 30;
 
     setInterval(function(){
-        currentTemp--;
-        var values = {
-            "measuredDate": new Date(),
-            "value": currentTemp
+        if(self.temp >= properTemp + range){ //max
+            console.log('change to cooling');
+            self.state = 'cooling';
+        }else if(self.temp <= properTemp - range){ //min
+            console.log('change to heating');
+            self.state = 'heating';
+        }else if(self.temp == properTemp){
+            console.log('change to normal');
+            self.state = 'normal';
         }
+
+        if(self.state == 'normal'){
+            self.temp = self.temp + self.type;
+        }else if(self.state == 'cooling'){
+            self.temp--;
+        }else if(self.state == 'heating'){
+            self.temp++;
+        }
+
+        var values = {
+            measuredDate: new Date(),
+            value: self.temp
+        };
         self.emit('measure', values);
     }, 3000);
 };
